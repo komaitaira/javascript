2806adf7b6b42af36ec35fae4fc9d924becb791f
komaitaira <komaitaira@gmail.com>
2020-05-19 15:00:03 +0900

    [start]6-2

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-alert("Hello World");
+var hello = "Hello World";
+alert(hello);
+
+// 整数を代入する
+var int1 = 1;
+
+// 負数を代入する
+var int2 = -10;
+
+// 小数点を代入する
+var float1 = 3.14;
+
+// 文字列を代入する
+var str1 = "JavaScriptを覚えよう";
